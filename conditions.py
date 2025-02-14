aba83cbe8af2242b4543ba9f9c82759236f456dc
karimfarha00 <karim_tech_edu@outlook.com>
2025-02-14 15:15:50 +0200

    Add files via upload

---
+++ conditions.py
@@ -0,0 +1,23 @@
+# Age logic:
+age = int(input("Please enter your age: "))
+
+check = age > 12
+
+if check:
+  print("You are eligible to run this code")
+else:
+  print("Not eligible")
+
+# Grades logic:
+# grade = int(input("Please enter your grade: "))
+
+# if grade >= 90:
+#   print("A")
+# elif grade >= 80:
+#   print("B")
+# elif grade >= 70:
+#   print("C")
+# elif grade >= 60:
+#   print("D")
+# else:
+#   print("F")
